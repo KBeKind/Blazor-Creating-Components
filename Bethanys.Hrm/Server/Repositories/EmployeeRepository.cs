ba3bd33c16a9b774e16fc06a9f50d983d56361fc
KBeKind <79383539+KBeKind@users.noreply.github.com>
2024-02-12 12:26:38 -0600

    commit

---
+++ Bethanys.Hrm/Server/Repositories/EmployeeRepository.cs
@@ -0,0 +1,39 @@
+﻿using Bethanys.Hrm.Shared;
+
+namespace Bethanys.Hrm.Server.Repositories
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private List<EmployeeModel> _employees = new List<EmployeeModel>
+        {
+            new EmployeeModel
+            {
+                EmployeeId = 1,
+                FirstName = "Bethany",
+                LastName = "Smith"
+            },
+            new EmployeeModel
+            {
+                EmployeeId = 2,
+                FirstName = "John",
+                LastName = "Baker"
+            },
+            new EmployeeModel
+            {
+                EmployeeId = 3,
+                FirstName = "Sal",
+                LastName = "Beneke"
+            }
+        };
+
+        public IEnumerable<EmployeeModel> GetAllEmployees()
+        {
+            return _employees;
+        }
+
+        public EmployeeModel GetEmployeeById(int employeeId)
+        {
+            return _employees.First(e => e.EmployeeId == employeeId);
+        }
+    }
+}
---
+++ Bethanys.Hrm/Shared/BenefitEmployeeModel.cs
@@ -0,0 +1,17 @@
+﻿using Bethanys.Components;
+
+namespace Bethanys.Hrm.Shared
+{
+    public class BenefitEmployeeModel: ITableModel
+    {
+        public int BenefitId { get; set; }
+
+        public bool Selected { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public DateTimeOffset? StartDate { get; set; }
+        public DateTimeOffset? EndDate { get; set; }
+        public bool Premium { get; set; }
+        public bool HighLightRow => Premium;
+        public bool ShowChildTemplate => false;
+    }
+}
